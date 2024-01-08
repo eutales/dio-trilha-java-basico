4856e9c672f0baad7b9a3f45846a42ad65d1213e
TALES SIQUEIRA <eu.tsiqueira@gmail.com>
2024-01-08 16:31:51 -0300

    Unario

---
+++ operadores/src/Unario.java
@@ -0,0 +1,22 @@
+public class Unario {
+    //Operador negação !
+    //Operador unário +, -, ++, --
+    public static void main(String[] args) {
+        //Operador unário, fazendo com que o número se torne negativo.  
+        int numero = 5;
+         numero=-numero;
+         System.out.println(numero);
+
+//Decremento do valor total;
+         numero = 5;
+         numero -= numero;
+         System.out.println(numero);
+//Incremento unitátio
+         numero = 5;
+         numero++; //Resgata o valor e incrementa.
+         System.out.println(numero);
+    }
+
+   
+
+}
