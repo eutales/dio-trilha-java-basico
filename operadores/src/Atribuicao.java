6cb45002fb58b2e4c8151706b13d9ad30b11b1b7
TALES SIQUEIRA <eu.tsiqueira@gmail.com>
2024-01-08 01:36:59 -0300

    Operadores - Aritmeticos e Atribuição de Valores

---
+++ operadores/src/Atribuicao.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Atribuicao {
+    //OPERADOR DE ATRIBUIÇÃO = 
+    int idade = 32;
+    double  peso = 96.4;
+    String nome = "TALES";
+    char sexo = 'M';
+    boolean doador = true;
+    Date dataNascimento = new Date();        
+}
--- tipos-variaveis/src/TiposEVariaveis.java
+++ tipos-variaveis/src/TiposEVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposEVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {   
+        //Tipos primitivos
+        byte idade = 127;
+        short ano = 32767;
+        float pi = 3.45F;
+        double salario = 4565.45;
+        boolean valueBoolean = true;
+        char valueChar = 'A';
+
+        // Classe STRING 
+        String cep = "31990585"; //Existem CEP's que começam com 0
+        String CPF = "07622258645"; //CPF pode começar com 0
+
+        //CONSTANTES - PALAVRA FINAL E NOME EM CAIXA ALTA.
+        final double VALORPI = 3.14;
+        
+
+
+
     }
 }
